e2b9752a6fa5291c3ae51657c64d4f5260ce2c49
Saravana Kumar <saravananbscitm@gmail.com>
2020-06-15 11:36:18 +0530

    Add test for aws s3

---
+++ tests/phpunit/AwsS3/IOServiceForAwsS3Test.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AwsS3;
+
+use PHPUnit\Framework\TestCase;
+
+class IOServiceForAwsS3Test extends TestCase
+{
+
+    public function testGetClient()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testAddFile()
+    {
+        $this->assertTrue(true);
+    }
+}
